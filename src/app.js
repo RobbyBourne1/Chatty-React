fdc9ed287dc221d447cba368bba6c8d99e8c4081
RobbyBourne1 <robbybourne1@gmail.com>
2018-06-01 13:14:33 -0400

    Stuck on getting adroid to open up

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -7,13 +6,54 @@ import {
   View,
 } from 'react-native';
 
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createHttpLink } from 'apollo-link-http';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
+import ReduxLink from 'apollo-link-redux';
+import { onError } from 'apollo-link-error';
+
+const URL = 'localhost:8080'; // set your comp's url here
+
+const store = createStore(
+  combineReducers({
+    apollo: apolloReducer,
+  }),
+  {}, // initial state
+  composeWithDevTools(),
+);
+
+const cache = new ReduxCache({ store });
+
+const reduxLink = new ReduxLink(store);
+
+const errorLink = onError((errors) => {
+  console.log(errors);
+});
+
+const httpLink = createHttpLink({ uri: `http://${URL}/graphql` });
+
+const link = ApolloLink.from([
+  reduxLink,
+  errorLink,
+  httpLink,
+]);
+
+export const client = new ApolloClient({
+  link,
+  cache,
+});
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
 
 export default class App extends Component {
   render() {
