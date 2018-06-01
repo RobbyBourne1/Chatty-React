28b0043170574023dd90b069f6f92c596cd49aea
RobbyBourne1 <robbybourne1@gmail.com>
2018-06-01 09:40:56 -0400

    GraphQL working and Starting react-native

--- server/data/schema.js
+++ server/data/schema.js
@@ -1,9 +1,9 @@
 export const Schema = [
-    `type Query {
-      testString: String
-    }
-    schema {
-      query: Query
-    }`,
-  ];
-  export default Schema;
+  `type Query {
+    testString: String
+  }
+  schema {
+    query: Query
+  }`,
+];
+export default Schema;
